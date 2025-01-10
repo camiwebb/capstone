c9cf5b17c64a51a137d4629a13d241fafb932f26
Camille Webb <camillewebb35@gmail.com>
2025-01-09 20:00:46 -0700

    set up home page with navbar and initial styling

--- src/client/Components/Account.jsx
+++ src/client/Components/Account.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = ()=> {
+const Account = ()=> {
   return (
     <div>
       <h1>Foo</h1>
@@ -8,4 +8,4 @@ const Home = ()=> {
   );
 };
 
-export default Home;
+export default Account;
--- src/client/Components/Home.jsx
+++ src/client/Components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = ()=> {
+  const navigate = useNavigate();
+
+  const goToRestStops = () => {
+    navigate('/rest-stops');
+  };
+
+
   return (
     <div>
-      <h1>Foo</h1>
+      <h1>Welcome to Utah Rest Stop Reviews</h1>
+      <p>Planning a roadtrip? Visiting family? Whatever takes you on the open road, find and review rest stops along the way.</p>
+      <button onClick={goToRestStops}>Check Out Available Rest Stops</button>
     </div>
   );
 };
